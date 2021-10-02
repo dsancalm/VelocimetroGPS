9c63a57d9d7e65824c6109db1ea9d19afa05eb4d
J5005\dsanc <dionisiosanchezalmagro@gmail.com>
2021-10-03 00:58:51 +0200

    primera puesta en marcha

--- app/src/main/java/es/dsancalm/velocimetrogps/MainActivity.java
+++ app/src/main/java/es/dsancalm/velocimetrogps/MainActivity.java
@@ -2,6 +2,7 @@ package es.dsancalm.velocimetrogps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.View;
@@ -13,12 +14,15 @@ public class MainActivity extends AppCompatActivity {
     Chronometer chronoTiempo;
     private long lastPause;
     private boolean isRunning = false;
+    final Location[] location1 = {null, null};
+    float distancia = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView textVelocidad = (TextView) findViewById(R.id.valueVelocidad);
         textVelocidad.setText("24,5 km/h");
+        GPSTracker mGPS = new GPSTracker(this);
         TextView textDistancia = (TextView) findViewById(R.id.valueDistancia);
         textDistancia.setText("235 metros");
         chronoTiempo = (Chronometer) findViewById(R.id.valueTiempo);
@@ -60,7 +64,23 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onChronometerTick(Chronometer chronometer) {
                 long timeElapsed = SystemClock.elapsedRealtime() - chronometer.getBase();
-                textDistancia.setText(String.valueOf(timeElapsed));
+
+                if(mGPS.canGetLocation() ){
+                    if (location1[0] == null){
+                        location1[0] = mGPS.getLocation();
+                    }
+                    else {
+                        location1[1] = mGPS.getLocation();
+                        float distanciaLocal = location1[0].distanceTo(location1[1]);
+                        float velocidad = (distanciaLocal*3600)/1000;
+                        distancia += distanciaLocal;
+                        location1[0] = location1[1];
+                        textVelocidad.setText(String.valueOf(distanciaLocal));
+                        textDistancia.setText(String.valueOf(distancia));
+                    }
+                }else{
+                    textDistancia.setText("Unabletofind");
+                }
             }
         });
     }
