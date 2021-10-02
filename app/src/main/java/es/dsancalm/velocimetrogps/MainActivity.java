6d7152be5e9f696a9a548d5ed81d72f6b1eb59b2
J5005\dsanc <dionisiosanchezalmagro@gmail.com>
2021-10-03 00:02:46 +0200

    Modelado de la vista

--- app/src/main/java/es/dsancalm/velocimetrogps/MainActivity.java
+++ app/src/main/java/es/dsancalm/velocimetrogps/MainActivity.java
@@ -3,6 +3,7 @@ package es.dsancalm.velocimetrogps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textVelocidad = (TextView) findViewById(R.id.valueVelocidad);
+        textVelocidad.setText("24,5 km/h");
+        TextView textDistancia = (TextView) findViewById(R.id.valueDistancia);
+        textDistancia.setText("235 metros");
+        TextView textTiempo = (TextView) findViewById(R.id.valueTiempo);
+        textTiempo.setText("00:02:57");
     }
 }
